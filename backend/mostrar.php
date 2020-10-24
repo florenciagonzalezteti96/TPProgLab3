278e1bc6f5c37670728866ffe42a5ffcc586732b
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2020-10-23 22:08:03 -0300

    Update mostrar.php

--- backend/mostrar.php
+++ backend/mostrar.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HTML 5 – Listado de Empleados</title>
-    <script src="../funciones.js"></script>
+    <script src="/funciones.js"></script>
 </head>
 
 <body>
     <header>
         <h2>Listado de Empleados</h2>
     </header>
-    <form name="formModificar" id="form" action="../index.php" method="POST">
+    <form name="formModificar" id="form" action="/index.php" method="POST">
         <table>
             <thead>
                 <th>
@@ -27,10 +27,10 @@
                 </tr>
                 <tr>
                     <?php
-                    require('../clases/empleado.php');
-                    include('./validarSesion.php');
+                    require_once('/clases/empleado.php');
+                    include('/validarSesion.php');
 
-                    $_pathDirectorioEmpleados = "../archivos";
+                    $_pathDirectorioEmpleados = "/archivos";
                     $_pathArchivoEmpleados = $_pathDirectorioEmpleados . "/empleados.txt";
 
                     if (is_dir($_pathDirectorioEmpleados)) {
@@ -77,7 +77,7 @@
                                 <img src='." . $empleadoNuevo->GetPathFoto() . "' alt='Foto de empleado' width='90px' height='90px'>
                                 </td>
                                 <td> 
-                                <a href='../eliminar.php?txtLegajo=" . $datosEmpleado[4] . "' method='GET'> Eliminar </a> 
+                                <a href='/eliminar.php?txtLegajo=" . $datosEmpleado[4] . "' method='GET'> Eliminar </a> 
                                 </td>
                                 <td> 
                                 <button id='btnModificar' name='btnModificar' type='button' onclick='AdministrarModificar(" . $empleadoNuevo->GetDni() . ")'>Modificar
@@ -104,8 +104,8 @@
         </table>
         <input type='hidden' id='hdnModificar' name='hdnModificar' value='vacio' />
     </form>
-    <a href="../index.php">Alta de Empleados</a>
-    <a href='./cerrarSesion.php'>Desloguearse</a>
+    <a href="/index.php">Alta de Empleados</a>
+    <a href='/cerrarSesion.php'>Desloguearse</a>
 </body>
 
 
