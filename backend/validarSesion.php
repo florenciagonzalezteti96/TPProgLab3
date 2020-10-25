459fa53234afd3307ffd390b34de8275d3eb0ec2
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2020-10-24 22:10:46 -0300

    Update validarSesion.php

--- backend/validarSesion.php
+++ backend/validarSesion.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION))
 {
     session_start();
 }
-else if(!isset($_SESSION['DNIEmpleado']))
+if(!isset($_SESSION['DNIEmpleado']))
 {
     "El login ha fallado, redirigiendo al login...";
     header("refresh:5;url= ../login.html");
