b3b700fab87aca4efe07b3139ad75068b6bfc453
florenciagonzalezteti96 <47353531+florenciagonzalezteti96@users.noreply.github.com>
2020-10-24 22:18:12 -0300

    Update cerrarSesion.php

--- backend/cerrarSesion.php
+++ backend/cerrarSesion.php
@@ -1,9 +1,9 @@
 <?php
 
-if (isset($_SESSION)) {
-    if (isset($_SESSION['DNIEmpleado'])) {
+if (isset($_SESSION) && isset($_SESSION['DNIEmpleado'])) {
+    
         session_unset();
         session_destroy();
-        header("Location: ../login.html");
-    }
+        header("Location: /login.html");
+   
 }
